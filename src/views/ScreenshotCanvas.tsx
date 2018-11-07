d5456cd71463e90c1d827adca615f40b1f021d98
TanninOne <seppleviathan@gmx.de>
2018-11-07 13:36:00 +0100

    fixed: very rare exception where reading screenshot fails
    this is rare because the file must have been readable a moment earlier.
    
    fixes #2079

--- src/views/ScreenshotCanvas.tsx
+++ src/views/ScreenshotCanvas.tsx
@@ -2,6 +2,7 @@ import { ISavegame } from '../types/ISavegame';
 
 import savegameLibInit, { Dimensions } from 'gamebryo-savegame';
 import * as React from 'react';
+import { log } from 'vortex-api';
 
 const savegameLib = savegameLibInit('savegameLib');
 
@@ -20,12 +21,16 @@ class ScreenshotCanvas extends React.Component<ICanvasProps, {}> {
     const imgData: ImageData = ctx.createImageData(
       Math.max(this.screenshotCanvas.width, 1), Math.max(this.screenshotCanvas.height, 1));
 
-    const sg = new savegameLib.GamebryoSaveGame(this.props.save.filePath);
-    sg.screenshot(imgData.data);
-    createImageBitmap(imgData)
-      .then((bitmap) => {
-        ctx.drawImage(bitmap, 0, 0);
-      });
+    try {
+      const sg = new savegameLib.GamebryoSaveGame(this.props.save.filePath);
+      sg.screenshot(imgData.data);
+      createImageBitmap(imgData)
+        .then((bitmap) => {
+          ctx.drawImage(bitmap, 0, 0);
+        });
+    } catch (err) {
+      log('warn', 'failed to read savegame screenshot', { fileName: this.props.save.filePath, error: err.message });
+    }
   }
 
   public render(): JSX.Element {
