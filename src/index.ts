Nexus-Mods/extension-gamebryo-savegames

commit 569b91583dcb33c7290940fd66e6688fab53f7f3

fixed: savegame list always refreshing on focus change

src/index.ts
@@ -11,6 +11,7 @@ import Settings from './views/Settings';
 
 import * as Promise from 'bluebird';
 import { remote } from 'electron';
+import * as _ from 'lodash';
 import * as path from 'path';
 import * as Redux from 'redux';
 import { actions, fs, log, selectors, types, util } from 'vortex-api';
@@ -32,6 +33,24 @@ function applySaveSettings(api: types.IExtensionApi,
   store.dispatch(setSavegamePath(savePath));
 }
 
+function saveDictEqual(lhs: { [id: string]: ISavegame },
+                       rhs: { [id: string]: ISavegame }): boolean {
+  if (!_.isEqual(Object.keys(lhs).sort(), Object.keys(rhs).sort())) {
+    return false;
+  }
+
+  const compareDate = (key) => {
+    const lDate = lhs[key].attributes.creationtime;
+    const rDate = rhs[key].attributes.creationtime;
+    if ((lDate === undefined) || (rDate === undefined)) {
+      return true;
+    }
+    return lDate.getTime() === rDate.getTime();
+  };
+
+  return Object.keys(lhs).find(key => !compareDate(key)) === undefined;
+}
+
 function updateSaves(store: Redux.Store<any>,
                      savesPath: string): Promise<string[]> {
   const newSavegames: ISavegame[] = [];
@@ -44,26 +63,26 @@ function updateSaves(store: Redux.Store<any>,
   .then(({ failedReads, truncated }) => Promise.resolve({ newSavegames, failedReads, truncated }))
   .then((result: { newSavegames: ISavegame[], failedReads: string[], truncated: boolean }) => {
     const savesDict: { [id: string]: ISavegame } = {};
-    result.newSavegames.forEach(
-      (save: ISavegame) => { savesDict[save.id] = save; });
+    result.newSavegames.forEach((save: ISavegame) => { savesDict[save.id] = save; });
 
-    store.dispatch(setSavegames(savesDict, result.truncated));
+    const state = store.getState();
+    const oldSaves: { [id: string]: ISavegame } = state.session.saves.saves;
+
+    if (!saveDictEqual(oldSaves, savesDict)) {
+      store.dispatch(setSavegames(savesDict, result.truncated));
+    }
     return Promise.resolve(result.failedReads);
   });
 }
 
-let missedUpdate: { profileId: string, savesPath: string };
-
 function genUpdateSavegameHandler(api: types.IExtensionApi) {
   return (profileId: string, savesPath: string) => {
     if (!remote.getCurrentWindow().isFocused()) {
-      missedUpdate = { profileId, savesPath };
       return Promise.resolve();
     }
 
     api.store.dispatch(actions.startActivity('savegames', 'Loading'));
 
-    missedUpdate = undefined;
     return updateSaves(api.store, savesPath)
       .then((failedReadsInner: string[]) => {
         if (failedReadsInner.length > 0) {
@@ -174,18 +193,6 @@ function once(context: types.IExtensionContext, update: util.Debouncer) {
   context.api.setStylesheet('savegame-management',
     path.join(__dirname, 'savegame_management.scss'));
 
-  const onFocus = () => {
-    if (missedUpdate !== undefined) {
-      update.schedule(undefined, missedUpdate.profileId, missedUpdate.savesPath);
-    }
-  };
-
-  remote.getCurrentWindow().on('focus', onFocus);
-
-  window.addEventListener('beforeunload', () => {
-    remote.getCurrentWindow().removeListener('focus', onFocus);
-  });
-
   context.api.onStateChange(['persistent', 'profiles'],
     (oldProfiles: { [profileId: string]: types.IProfile },
      newProfiles: { [profileId: string]: types.IProfile }) => {
@@ -220,6 +227,8 @@ function once(context: types.IExtensionContext, update: util.Debouncer) {
       store.dispatch(setSavegamePath(savePath));
     }
   }
+
+  updateSavegames(context.api, update);
 }
 
 function init(context: IExtensionContextExt): boolean {

src/stylesheets/savegame_management.scss
@@ -28,15 +28,18 @@
 }
 
 .savegames-busy-panel {
-  height: 100%;
-  .panel-body {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    svg {
-      width: 3em;
-      height: 3em;
-    }
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  svg {
+    width: 3em;
+    height: 3em;
   }
 }

src/views/SavegameList.tsx
@@ -128,7 +128,8 @@ class SavegameList extends ComponentEx<Props, IComponentState> {
           {header}
         </MainPage.Header>
         <MainPage.Body>
-          {activity.length > 0 ? this.renderBusy() : this.renderContent(saveActions)}
+          {this.renderContent(saveActions)}
+          {activity.length > 0 ? this.renderBusy() : null}
         </MainPage.Body>
       </MainPage>
     );
@@ -178,14 +179,10 @@ class SavegameList extends ComponentEx<Props, IComponentState> {
     if (activity.length > 0) {
       const PanelX: any = Panel;
       return (
-        <FlexLayout.Fixed className='savegames-busy-panel'>
-          <Panel>
-            <PanelX.Body>
-              <Spinner />
-              {t(activity[0])}
-            </PanelX.Body>
-          </Panel>
-        </FlexLayout.Fixed>
+        <div className='savegames-busy-panel'>
+          <Spinner />
+          {t(activity[0])}
+        </div>
       );
     } else {
       return null;

commit ac338f510b6f315c42b8e3641d52d778a87babc6

fixed exception on refreshing savegame list
introduced in 569b91583dcb33c7290940fd66e6688fab53f7f3 so wasn't released

src/index.ts
@@ -40,11 +40,18 @@ function saveDictEqual(lhs: { [id: string]: ISavegame },
   }
 
   const compareDate = (key) => {
-    const lDate = lhs[key].attributes.creationtime;
-    const rDate = rhs[key].attributes.creationtime;
+    let lDate = lhs[key].attributes.creationtime;
+    let rDate = rhs[key].attributes.creationtime;
     if ((lDate === undefined) || (rDate === undefined)) {
       return true;
     }
+    if (typeof(lDate) === 'string') {
+      lDate = new Date(lDate);
+    }
+    if (typeof(rDate) === 'string') {
+      rDate = new Date(rDate);
+    }
+
     return lDate.getTime() === rDate.getTime();
   };
 