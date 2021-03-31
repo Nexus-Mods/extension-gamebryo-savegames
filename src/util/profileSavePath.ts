607249a50b550cdc140ad9513154fbd6ad2874e0
idcs <idc.dts@gmail.com>
2021-03-31 07:26:03 +0100

    fixed incorrect save game paths for enderal SE
    fixes nexus-mods/vortex#8961

--- src/util/profileSavePath.ts
+++ src/util/profileSavePath.ts
@@ -7,6 +7,10 @@ export function profileSavePath(profile: types.IProfile, forceGlobal: boolean = 
     return localSaves && !forceGlobal
       ? path.join('..', 'Enderal', 'Saves', profile.id) + path.sep
       : path.join('..', 'Enderal', 'Saves') + path.sep;
+  } else if (profile.gameId === 'enderalspecialedition') {
+    return localSaves && !forceGlobal
+      ? path.join('..', 'Enderal Special Edition', 'Saves', profile.id) + path.sep
+      : path.join('..', 'Enderal Special Edition', 'Saves') + path.sep;
   } else {
     return localSaves && !forceGlobal
       ? path.join('Saves', profile.id) + path.sep
