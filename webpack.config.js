3e5c4449c433a71584d54d6d8db69e9a1b3e2ab4
TanninOne <seppleviathan@gmx.de>
2022-03-30 11:41:06 +0200

    updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 let webpack = require('vortex-api/bin/webpack').default;
 
-const res = webpack('gamebryo-savegame-management', __dirname, 4);
+const res = webpack('gamebryo-savegame-management', __dirname, 5);
 
 res.externals['./GamebryoSave'] = './GamebryoSave';
 
